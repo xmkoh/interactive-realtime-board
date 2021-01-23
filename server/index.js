7468705057d55e643acd843af1137f94fb21e436
x_ming <kohxianming@hotmail.com>
2021-01-23 18:55:22 +0800

    m

--- server/index.js
+++ server/index.js
@@ -1,4 +1,6 @@
-const app = require('express')();
+const express = require('express');
+const app = express();
+const path = require('path');
 const server = require('http').createServer(app);
 const io = require('socket.io')(server, {
       cors: {
