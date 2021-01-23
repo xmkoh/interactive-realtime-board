605f757cfa7be899ca10995d25e6084c0b678fab
x_ming <kohxianming@hotmail.com>
2021-01-23 18:41:57 +0800

    m

---
+++ server/index.js
@@ -0,0 +1,25 @@
+const app = require('express')();
+const server = require('http').createServer(app);
+const io = require('socket.io')(server, {
+      cors: {
+        origin: '*',
+      }
+    });
+
+app.use(require("cors")())
+
+let tempImage;
+
+io.on('connection', (socket)=> {
+      console.log('User Online');
+      socket.on('canvas-data', (data)=> {
+            socket.broadcast.emit('canvas-data', data);
+            tempImage = data;
+      })
+      socket.broadcast.emit('canvas-data', tempImage)
+})
+
+const server_port = process.env.YOUR_PORT || process.env.PORT || 5000;
+server.listen(server_port, () => {
+    console.log("Started on : "+ server_port);
+})
---
+++ ui/src/components/container/Container.jsx
@@ -0,0 +1,136 @@
+import React from 'react';
+import Board from '../board/Board';
+
+import './style.css';
+
+class Container extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            color: "#000000",
+            size: "8"
+        }
+    }
+
+    changeColor(params) {
+        this.setState({
+            color: params.target.value
+        })
+    }
+
+    setColor(color) {
+        this.setState({
+            color: color
+        })
+    }
+
+    changeSize(pixel) {
+        console.log("CHANGE SIZE")
+        this.setState({
+            size: pixel
+        })
+    }
+
+    render() {
+
+        return (
+            <div className="container">
+                <div className="tools-section">
+                    Select Brush Color : &nbsp;
+                    <input className="toolbox-group" type="color" id="color-picker" value={this.state.color} onChange={this.changeColor.bind(this)} />
+                    <div className="color-picker-container toolbox-group">
+                        <div className="color-button-row">
+                            <button className="color-button" style={{ backgroundColor: '#ffffff' }}
+                                onClick={() => this.setColor('#ffffff')}></button>
+                            <button className="color-button" style={{ backgroundColor: '#c1c1c1' }}
+                                onClick={() => this.setColor('#c1c1c1')}></button>
+                            <button className="color-button" style={{ backgroundColor: '#ef130b' }}
+                                onClick={() => this.setColor('#ef130b')}></button>
+                            <button className="color-button" style={{ backgroundColor: '#ff7100' }}
+                                onClick={() => this.setColor('#ff7100')}></button>
+                            <button className="color-button" style={{ backgroundColor: '#ffe400' }}
+                                onClick={() => this.setColor('#ffe400')}></button>
+                            <button className="color-button" style={{ backgroundColor: '#00cc00' }}
+                                onClick={() => this.setColor('#00cc00')}></button>
+                            <button className="color-button" style={{ backgroundColor: '#00b2ff' }}
+                                onClick={() => this.setColor('#00b2ff')}></button>
+                            <button className="color-button" style={{ backgroundColor: '#231fd3' }}
+                                onClick={() => this.setColor('#231fd3')}></button>
+                            <button className="color-button" style={{ backgroundColor: '#a300ba' }}
+                                onClick={() => this.setColor('#a300ba')}></button>
+                            <button className="color-button" style={{ backgroundColor: '#d37caa' }}
+                                onClick={() => this.setColor('#d37caa')}></button>
+                            <button className="color-button" style={{ backgroundColor: '#a0522d' }}
+                                onClick={() => this.setColor('#a0522d')}></button>
+                        </div>
+                        <div className="color-button-row">
+                            <button className="color-button" style={{ backgroundColor: '#000000' }}
+                                onClick={() => this.setColor('#000000')}></button>
+                            <button className="color-button" style={{ backgroundColor: '#4c4c4c' }}
+                                onClick={() => this.setColor('#4c4c4c')}></button>
+                            <button className="color-button" style={{ backgroundColor: '#740b07' }}
+                                onClick={() => this.setColor('#740b07')}></button>
+                            <button className="color-button" style={{ backgroundColor: '#c23800' }}
+                                onClick={() => this.setColor('#c23800')}></button>
+                            <button className="color-button" style={{ backgroundColor: '#e8a200' }}
+                                onClick={() => this.setColor('#e8a200')}></button>
+                            <button className="color-button" style={{ backgroundColor: '#005510' }}
+                                onClick={() => this.setColor('#005510')}></button>
+                            <button className="color-button" style={{ backgroundColor: '#00569e' }}
+                                onClick={() => this.setColor('#00569e')}></button>
+                            <button className="color-button" style={{ backgroundColor: '#0e0865' }}
+                                onClick={() => this.setColor('#0e0865')}></button>
+                            <button className="color-button" style={{ backgroundColor: '#550069' }}
+                                onClick={() => this.setColor('#550069')}></button>
+                            <button className="color-button" style={{ backgroundColor: '#a75574' }}
+                                onClick={() => this.setColor('#a75574')}></button>
+                            <button className="color-button" style={{ backgroundColor: '#63300d' }}
+                                onClick={() => this.setColor('#63300d')}></button>
+                        </div>
+                    </div>
+
+                    <div className="pencil-sizes-container toolbox-group">
+                        <label for="size8">
+                            <input id="size8" className="custom-check-or-radio line-width-button" onChange={() => this.changeSize(8)} type="radio" name="line-width" />
+                            <div className="line-width-button-content">
+                                <div style={{ height: '8px', width: '8px' }} className="dot"></div>
+                            </div>
+                        </label>
+                        <label for="size16">
+                        <input id="size16" className="custom-check-or-radio line-width-button" onChange={() => this.changeSize(16)} type="radio" name="line-width" />
+                        <div
+                            className="line-width-button-content">
+                            <div style={{ height: '16px', width: '16px' }} className="dot"></div>
+                        </div>
+                        </label>
+                        <label for="size24">
+                        <input id="size24" className="custom-check-or-radio line-width-button" onChange={() => this.changeSize(24)} type="radio" name="line-width" />
+                        <div
+                            
+                            className="line-width-button-content">
+                            <div style={{ height: '24px', width: '24px' }} className="dot"></div>
+                        </div>
+                        </label>
+                        <label for="size32">
+                        <input id="size32" className="custom-check-or-radio line-width-button" onChange={() => this.changeSize(32)} type="radio" name="line-width" />
+                        <div
+                            
+                            className="line-width-button-content">
+                            <div style={{ height: '32px', width: '32px' }} className="dot"></div>
+                        </div>
+                        </label>
+                    </div>
+
+
+                </div>
+
+                <div className="board-container">
+                    <Board color={this.state.color} size={this.state.size}></Board>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default Container
